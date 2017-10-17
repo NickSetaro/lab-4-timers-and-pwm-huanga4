09901ce2d8f98132d249890847d7247b3b9fb663
huanga4 <32047022+huanga4@users.noreply.github.com>
2017-10-16 20:53:04 -0400

    Update main.c

--- Debouncing/Debouncing(2311)/main.c
+++ Debouncing/Debouncing(2311)/main.c
@@ -7,43 +7,39 @@
  * Debouncing 2311
  */
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog 
 
-    P1DIR |=BIT0; //set Port 1.0 output ---LED
+    P1DIR |=BIT0; //set Port 1.0 output LED
 
-    P1DIR &=~(BIT1); //set Port 1.1 input --- pushbutton
-    P1REN|=BIT1;//enable pull-up/pull-down resistor on
-    P1OUT|=BIT1; //choose the pull-up resistor
+    P1DIR &=~(BIT1); //set Button
+    P1REN|=BIT1;//enable pull-up/pull-down 
+    P1OUT|=BIT1; //choose pull-up 
 
-    P1IE |=BIT1;//enable the interrupt on Port 1.1
-    P1IES |=BIT1;//set as falling edge
-    P1IFG &=~(BIT1);//clear interrupt flag
+    P1IE |=BIT1;//enable interrupt 
+    P1IES |=BIT1;//set falling edge
+    P1IFG &=~(BIT1);//clear flag
 
-    //enter LPM4 mode and enable global interrupt
-    _BIS_SR(LPM4_bits + GIE);
+    _BIS_SR(LPM4_bits + GIE); //Low Power Mode 4
 }
-//Port 1 ISR
+
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT_1(void)
 {
-    P1OUT ^=0x01; // Change state of P1.1
-    P1IE &= ~BIT1; // Disable interrupt
+    P1OUT ^=0x01; //Toggle
+    P1IE &= ~BIT1; // disable interrupt
 
     //Debounce 1
     __delay_cycles(1);
 
     //Debounce 2
-    TB0CTL = TBSSEL_1 + MC_1 + ID_1; //Set up Timer B, Count up, divider 2
-    TB0CCTL0 = 0x10; // Set up compare mode for CCTL
-    TB0CCR0 = 1000; // Duration for which the interrupt is disable
-                    // Duration 1000/16kHz = 1/16 sec.
+    TB0CTL = TBSSEL_1 + MC_1 + ID_1; //Set up Timer B, Count up, divider value 2
+    TB0CCTL0 = 0x10; // set up compare mode
+    TB0CCR0 = 1000; // interrupt is disable; duration disabled = 1000/16kHz = 1/16 sec
 
     P1IFG &=~(BIT1); // Clear flag
 }
 #pragma vector=TIMER0_B0_VECTOR
 __interrupt void Timer_B0(void)
 {
-
     P1IE |= BIT1; //Enable interrupt again.
-
 }
