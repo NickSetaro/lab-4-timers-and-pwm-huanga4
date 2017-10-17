dcd4b2397a565bfdc44e706b7198e31ef53195e0
huanga4 <32047022+huanga4@users.noreply.github.com>
2017-10-16 21:04:05 -0400

    Update main.c

--- Debouncing/Debouncing(6989)/main.c
+++ Debouncing/Debouncing(6989)/main.c
@@ -7,39 +7,35 @@
  * Debouncing 6989
  */
 void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog timer
-
+    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
 
-    P1DIR |=BIT0; //set Port 1.0 output ---LED
+    P1DIR |=BIT0; //set Port 1.0 output LED
 
-    P1DIR &=~(BIT1); //set Port 1.1 input --- pushbutton
-    P1REN|=BIT1;//enable pull-up/pull-down resistor on
-    P1OUT|=BIT1; //choose the pull-up resistor
+    P1DIR &=~(BIT1); //set Button
+    P1REN|=BIT1;//enable pull-up/pull-down 
+    P1OUT|=BIT1; //choose pull-up 
 
-    P1IE |=BIT1;//enable the interrupt on Port 1.1
-    P1IES |=BIT1;//set as falling edge
-    P1IFG &=~(BIT1);//clear interrupt flag
+    P1IE |=BIT1;//enable interrupt 
+    P1IES |=BIT1;//set falling edge
+    P1IFG &=~(BIT1);//clear flag
 
-    //enter LPM4 mode and enable global interrupt
-    _BIS_SR(LPM4_bits + GIE);
+    _BIS_SR(LPM4_bits + GIE); //Low Power Mode 4
 }
-//Port 1 ISR
+
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT_1(void)
 {
-    P1OUT ^=0x01; // Change state of P1.1
-    P1IE &= ~BIT1; // Disable interrupt
+    P1OUT ^=0x01; // Toggle
+    P1IE &= ~BIT1; // disable interrupt
 
     //Debounce 1
     __delay_cycles(1);
 
     //Debounce 2
-    TA0CTL = TASSEL_1 + MC_1 + ID_1; //Set up Timer A, Count up, divider 2
-    TA0CCTL0 = 0x10; // Set up compare mode for CCTL
-    TA0CCR0 = 1000; // Duration at which the interrupt is disable
-                    // Duration 1000/16kHz = 1/16 sec.
+    TA0CTL = TASSEL_1 + MC_1 + ID_1; //Set up Timer A, Count up, divider value 2
+    TA0CCTL0 = 0x10; // set up compare mode
+    TA0CCR0 = 1000; // interrupt is disabled; duration disabled = 1000/16kHz = 1/16 sec
 
     P1IFG &=~(BIT1); // Clear flag
 }
@@ -47,6 +43,6 @@ __interrupt void PORT_1(void)
 __interrupt void Timer_A0(void)
 {
 
-    P1IE |= BIT1; //Enable interrupt again.
+    P1IE |= BIT1; //re-enable interrupt
 
 }
