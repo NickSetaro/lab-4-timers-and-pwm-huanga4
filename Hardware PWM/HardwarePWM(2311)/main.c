d4b53360671ce2efd5bb56deb48e46ccf0fde965
huanga4 <32047022+huanga4@users.noreply.github.com>
2017-10-16 21:18:09 -0400

    Update main.c

--- Hardware PWM/HardwarePWM(2311)/main.c
+++ Hardware PWM/HardwarePWM(2311)/main.c
@@ -9,46 +9,45 @@
 volatile unsigned int f;
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
 
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
-    P1OUT &= ~BIT0;                         // Switch LED off
+    P1DIR |= BIT0;                          // set P1.0 LED
+    P1OUT &= ~BIT0;                         // clear P1.0
 
 
-    P2DIR |=BIT0;                           //set Port 2.0 output ---LED
-    P2OUT &= ~BIT0;                         //Clear P2.0
+    P2DIR |=BIT0;                           //set Port 2.0 LED
+    P2OUT &= ~BIT0;                         //clear P2.0
 
-    P1DIR  &= ~BIT1;                        // Set P1.1 as input
-    P1OUT |= BIT1;                          // Configure P1.1 for Pull-Up
-    P1REN |= BIT1;                          // Enable Pull Up of P1.1
+    P1DIR  &= ~BIT1;                        // set P1.1
+    P1OUT |= BIT1;                          // configure pull-up
+    P1REN |= BIT1;                          // enable pull-up
 
-    TB0CCTL1 = OUTMOD_7;                    // Reset/Set Mode
-    TB0CTL = TBSSEL_2 + MC_1 +TBCLR ;       // SMCLK / Upmode
-    TB0CCR0 = 100-1;                        // PWM Frequency 10 kHz
+    TB0CCTL1 = OUTMOD_7;                    // reset/set
+    TB0CTL = TBSSEL_2 + MC_1 +TBCLR ;       // TimerB set up, Up mode, SMCLK
+    TB0CCR0 = 100-1;                        
     TB0CCR1 = 50;                           // 50% Duty Cycle
 
 
     while(1)
     {
-        if(!(P1IN & BIT1)) //If the button is pressed
+        if(!(P1IN & BIT1)) //button pressed
         {
             P2OUT |= BIT0; //Sets P9.4
-            if(TB0CCR1 <= 90) // If the brightness is <= than 90%
-            {
-                TB0CCR0 = 0; // Reset CCR0
-                TB0CCR1 += 10; // Add 10%
-                TB0CCR0 = 100; // Set CCR0 back to 10 kHz
+            if(TB0CCR1 <= 90){ //Check brightness below 90%
+                TB0CCR0 = 0; // reset CCR0
+                TB0CCR1 += 10; // add 10
+                TB0CCR0 = 100; // assign CCR0 value
             }
-            else if (TB0CCR1 == 100){ // If the brightness is at 100%
-                TB0CCR0 = 0; // Reset CCR0
-                TB0CCR1 = 0; // Reset CCR1
-                TB0CCR0 = 100; //  Set CCR0 back to 10 kHz
+            else if (TB0CCR1 == 100){ // Check if brightness is max
+                TB0CCR0 = 0; //reset CCR0
+                TB0CCR1 = 0; //reset CCR1
+                TB0CCR0 = 100; //assign CCR0 value
             }
         }
-        if((P1IN & BIT1))
+        if((P1IN & BIT1)){
             P2OUT &= ~BIT0; //Clear P9.4
+        }
         // Debounce
         for(j=100;j>0;j--)
         {
